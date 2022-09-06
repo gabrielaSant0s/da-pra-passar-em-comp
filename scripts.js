05958cd6d1e31b586ea1d062a45413555f5df34d
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-09-06 18:51:35 -0300

    calcula para ciencia da computação tambem

--- scripts.js
+++ scripts.js
@@ -5,6 +5,10 @@ function clicar(){
     var hum = document.querySelector(".humanas").value
     var red = document.querySelector(".redacao").value
 
-    var resultado = (parseFloat(mat)*5+ parseFloat(nat)*4 +parseFloat(ling)*2 +parseFloat(hum)*1+ parseFloat(red)*3)/15
-    document.querySelector(".resultado").innerHTML = resultado
+    var resultadoEngUFRJ = (parseFloat(mat)*5+ parseFloat(nat)*4 +parseFloat(ling)*2 +parseFloat(hum)*1+ parseFloat(red)*3)/15
+    var resultadoCCUFRJ = (parseFloat(mat)*5+ parseFloat(nat)*4 +parseFloat(ling)*5 +parseFloat(hum)*3+ parseFloat(red)*3)/20
+    var resultadoCCCefet = (parseFloat(mat)*4+ parseFloat(nat)*3 +parseFloat(ling)*3 +parseFloat(hum)*1+ parseFloat(red)*3)/14
+    document.querySelector(".resultado").innerHTML = `eng de comp UFRJ : ${resultadoEngUFRJ} ||
+    CC ufrj: ${resultadoCCUFRJ} ||
+    CC Cefet : ${resultadoCCCefet}`
 }
